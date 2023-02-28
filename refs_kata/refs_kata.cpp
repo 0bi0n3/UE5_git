cd12298be814b072e36c603e198829f0cdf1953c
Oberon DW <oberon@one-audio.co.uk>
2023-02-28 17:05:35 +0000

    references kata added

---
+++ refs_kata/refs_kata.cpp
@@ -0,0 +1,9 @@
+// refs_kata.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
