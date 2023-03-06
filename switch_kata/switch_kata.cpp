3f1ad45b383c393f9873642ac21d7d89bebe8baf
Oberon DW <oberon@one-audio.co.uk>
2023-03-06 15:25:43 +0000

    enum and functions

--- switch_kata/switch_kata.cpp
+++ switch_kata/switch_kata.cpp
@@ -16,22 +16,45 @@ const float CrouchSpeed = 350.f;
 
 void UpdateMovementSpeed(PlayerStatus P_Status, float& speed);
 
+void SwitchOnInt(int i);
+
 int main()
 {
+	int numberchoice = 2;
+	SwitchOnInt(numberchoice);
+
 	float MovementSpeed;
 
-	PlayerStatus status = PS_crouching;
+	PlayerStatus status = PS_running;
 
 	UpdateMovementSpeed(status, MovementSpeed);
 
 	std::cout << "MovementSpeed = " << MovementSpeed << std::endl;
 
-
 	std::cin.get();
+}
+
+void SwitchOnInt(int i)
+{
+	switch (i)
+	{
+	case 0:
+		std::cout << "Your number was zero. \n";
+		break;
+	case 1:
+		std::cout << "Your number was one! \n";
+		break;
+	case 2:
+		std::cout << "Your number was two!! \n";
+		break;
+	default:
+		std::cout << "Your number was not valid... \n";
+	}
 }
 
 void UpdateMovementSpeed(PlayerStatus P_Status, float& speed)
 {
+	/*
 	if (P_Status == PS_running)
 	{
 		speed = RunSpeed;
@@ -42,6 +65,19 @@ void UpdateMovementSpeed(PlayerStatus P_Status, float& speed)
 	}
 	else if (P_Status == PS_crouching)
 	{
+		speed = CrouchSpeed;
+	}
+	*/
+
+	switch (P_Status)
+	{
+	case PS_running:
+		speed = RunSpeed;
+		break;
+	case PS_walking:
+		speed = WalkSpeed;
+		break;
+	case PS_crouching:
 		speed = CrouchSpeed;
 	}
 }
