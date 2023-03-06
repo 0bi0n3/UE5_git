b91c17e7a8675de53ce0c6ecfc56d01e6b22637b
Oberon DW <oberon@one-audio.co.uk>
2023-03-06 12:06:53 +0000

    added strings and char kata

--- strings_kata/strings_kata.cpp
+++ strings_kata/strings_kata.cpp
@@ -1,20 +1,18 @@
 // strings_kata.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <string>
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    char myCString[6] = { 'F', 'r', 'o', 'g', 's', '\0' };
+    std::string myCPPString;
+
+    myCPPString = "Ponds full of water!";
+
+    std::cout << myCString << std::endl;
+    std::cout << myCPPString << std::endl;
+
+
+    std::cin.get();
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
